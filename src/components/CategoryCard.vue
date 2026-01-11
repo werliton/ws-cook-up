30200c7d9d51ab66a725f52ad96154bf9f2f37da
Werliton Silva <werliton.silva@osf.digital>
2026-01-10 22:11:00 -0300

    feat: import images dynamicly

--- src/components/CategoryCard.vue
+++ src/components/CategoryCard.vue
@@ -11,5 +11,61 @@ export default {
 };
 </script>
 <template>
-  {{ categoria.nome }}
+  <article class="categoria">
+    <header class="categoria__cabecalho">
+      <img
+        :src="`/imagens/icones/categorias_ingredientes/${categoria.imagem}`"
+        :alt="`{{ categoria.nome }}`"
+        class="categoria__imagem"
+      />
+      <!-- {{ categoria.imagem }} -->
+      <h2 class="paragrafo-lg categoria__nome">{{ categoria.nome }}</h2>
+    </header>
+
+    <ul class="categoria__ingredientes">
+      <li v-for="ingrediente in categoria.ingredientes" :key="ingrediente">
+        {{ ingrediente }}
+      </li>
+    </ul>
+  </article>
 </template>
+
+<style scoped>
+.categoria {
+  width: 19.5rem;
+  padding: 1rem;
+  border-radius: 1rem;
+  background: var(--branco, #fff);
+  box-shadow: 4px 4px 10px 0px rgba(68, 68, 68, 0.05);
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+}
+
+.categoria__cabecalho {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.categoria__imagem {
+  width: 3.5rem;
+}
+
+.categoria__nome {
+  text-align: center;
+  color: var(--verde-medio, #3d6d4a);
+  font-weight: 700;
+}
+
+.categoria__ingredientes {
+  display: flex;
+  justify-content: center;
+  gap: 0.5rem;
+  flex-wrap: wrap;
+}
+</style>
