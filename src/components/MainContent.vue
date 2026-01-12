9d47676a60d5a5a7e314c0223610da8fbd12af75
Werliton Silva <werliton.silva@osf.digital>
2026-01-11 21:37:13 -0300

    feat: create new component

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,6 +1,6 @@
 <script>
 import SelectIngredients from "./SelectIngredients.vue";
-import Tag from "./Tag.vue";
+import YourList from "./YourList.vue";
 
 export default {
   data() {
@@ -8,29 +8,12 @@ export default {
       ingredientes: ["Alho", "Manteiga", "Orégano"],
     };
   },
-  components: { SelectIngredients, Tag },
+  components: { SelectIngredients, YourList },
 };
 </script>
 <template>
   <main class="conteudo-principal">
-    <section>
-      <span class="subtitulo-lg sua-lista-texto"> Sua lista: </span>
-
-      <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
-        <li v-for="ingrediente in ingredientes" :key="ingrediente">
-          <Tag :texto="ingrediente" ativa />
-        </li>
-      </ul>
-
-      <p v-else class="paragrafo lista-vazia">
-        <img
-          src="../assets/images/icones/lista-vazia.svg"
-          alt="Ícone de pesquisa"
-        />
-        Sua lista está vazia, selecione ingredientes para iniciar.
-      </p>
-    </section>
-
+    <YourList :ingredientes="ingredientes" />
     <SelectIngredients />
   </main>
 </template>
@@ -46,31 +29,6 @@ export default {
   flex-direction: column;
   align-items: center;
   gap: 5rem;
-}
-
-.sua-lista-texto {
-  color: var(--coral, #f0633c);
-  display: block;
-  text-align: center;
-  margin-bottom: 1.5rem;
-}
-
-.ingredientes-sua-lista {
-  display: flex;
-  justify-content: center;
-  gap: 1rem 1.5rem;
-  flex-wrap: wrap;
-}
-
-.lista-vazia {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 0.25rem;
-
-  color: var(--coral, #f0633c);
-  text-align: center;
 }
 
 @media only screen and (max-width: 1300px) {
